a80cb11d0fbf09be64e5162051ecd53d3f50e822
neyoung <ngozi.eze.young@gmail.com>
2021-07-23 10:04:12 -0400

    version 2
    Added score board to the game

--- app.js
+++ app.js
@@ -5,18 +5,25 @@
  */
 const playtoSelect = document.querySelector('#playto');
 const resetButton = document.querySelector('#reset');
+const winnerMsg = document.querySelector('#winner');
+const scoreBoard = document.querySelector('#scoreBoard');
 let winningScore = parseInt(playtoSelect.value);
 let isGameOver = false;
+let gameRound = 1;
 
 const player1 = {
-    score:0,
+    id: 1,
+    score: 0,
     display: document.querySelector('#p1Display'),
-    button: document.querySelector('#p1Button')
+    button: document.querySelector('#p1Button'),
+    scores: [],
 };
 const player2 = {
-    score:0,
+    id: 2,
+    score: 0,
     display: document.querySelector('#p2Display'),
-    button: document.querySelector('#p2Button')
+    button: document.querySelector('#p2Button'),
+    scores: [],
 };
 
 /* Updates the score and score display depending on which player button was clicked
@@ -32,12 +39,50 @@ function updateScores(player, opponent) {
             isGameOver = true;
             player.display.classList.add('has-text-success');
             opponent.display.classList.add('has-text-danger');
+            updateScoreBoard(player, opponent);
         }
         player.display.textContent = player.score;
     }
 }
+// Update score board after a player wins a game
+function updateScoreBoard(player, opponent) {
+    scoreBoard.rows[gameRound].cells[player.id].innerText = player.score;
+    scoreBoard.rows[gameRound].cells[opponent.id].innerText = opponent.score;
+    player.scores.push(1);
+    opponent.scores.push(0);
+    gameRound += 1;
+    if(gameRound > 3) {
+        annouceWinner(player, opponent);
+    }
+}
 
-// Resets the game and score display
+// Displays to screen the player that wins best out of three games
+function annouceWinner(player, opponent) {
+    let winnerId = (tally(player.scores) > tally(opponent.scores)) ? player.id : opponent.id;
+    winnerMsg.textContent = `***Player ${winnerId} is the winner!!!***`
+}
+
+function tally(scores) {
+    let total = 0;
+    for(let score of scores) {
+        total += score;
+    }
+    return total;
+}
+
+// Resets only the score board after playing three games
+function resetScoreBoard() {
+    gameRound = 1;
+    for(let i=1; i<scoreBoard.rows.length; i++) {
+        for(let j=1; j<scoreBoard.rows[i].cells.length; j++)
+            scoreBoard.rows[i].cells[j].textContent = '-';
+    }
+    player1.scores = [];
+    player2.scores = [];
+    winnerMsg.textContent = '';
+}
+
+// Resets only the score display when reset button is clicked
 function reset() {
     const players = [player1, player2];
     isGameOver = false;
@@ -46,6 +91,7 @@ function reset() {
         player.display.textContent = 0;
         player.display.classList.remove('has-text-success', 'has-text-danger');
     }
+    if(gameRound > 3) resetScoreBoard()
 }
 
 // Player One and Play Two buttons events
